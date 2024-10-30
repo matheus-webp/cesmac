ffb37f9b095bc0a638d747d55fab70d23b9b6b94
matheus-webp <matheus.webp@gmail.com>
2024-10-30 00:29:22 -0300

    feat: add ticket quantity option

--- POO/Main.java
+++ POO/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         Movies movies = new Movies();
@@ -19,9 +21,11 @@ public class Main {
         }
 
 
-        TicketOption ticketOption = ticket.ticketSelection();
-        System.out.printf("Detalhes do bilhete:\nFilme: %s\nSessão: %s\nEntrada: %s\nValor: %.2f", movie, sessionSelected, ticketOption, ticketOption.getPrice());
-        System.out.println("Boleto para pagamento: ");
+        ticket.ticketSelection();
+        HashMap<TicketOption, Double> ticketMap = ticket.getTicket();
+        TicketOption ticketOption = ticketMap.keySet().iterator().next();
+        double value= ticket.getValue();
 
+        System.out.printf("Detalhes do bilhete:\nFilme: %s\nSessão: %s\nEntrada: %s\nValor: %.2f\n", movie, sessionSelected, ticketOption, value);
     }
 }
--- POO/Ticket.java
+++ POO/Ticket.java
@@ -1,39 +1,55 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Ticket {
+    private HashMap<TicketOption, Double> ticket = new HashMap<>();
     private TicketOption ticketOption;
+    private int quantity;
+    public double value;
 
-    public TicketOption ticketSelection() {
+    public void ticketSelection() {
         Scanner scanner = new Scanner(System.in);
+
         boolean looping = true;
         while (looping) {
-            System.out.println("Digite 0 para sair");
             System.out.println("Escolha o tipo do ingresso:\n 1 - Inteira\n 2 - Meia");
-
             int ticketType = scanner.nextInt();
+            System.out.println("Digite a quantidade de ingressos :");
+            int ticketQuantity = scanner.nextInt();
+            if(ticketQuantity < 1){
+                System.out.println("A quantidade precisa ser igual ou maior que 1 ingresso");
+                continue;
+            }
 
             switch (ticketType) {
-                case 0:
-                    System.out.println("Compra de ingressos encerrada!");
-                    looping = false;
-                    break;
                 case 1:
                     ticketOption = TicketOption.INTEIRA;
+                    this.value = totalValueCalculate(ticketQuantity, ticketOption);
+                    this.ticket.put(ticketOption, value);
                     looping = false;
                     break;
                 case 2:
                     ticketOption = TicketOption.MEIA;
+                    this.value = totalValueCalculate(ticketQuantity, ticketOption);
+                    this.ticket.put(ticketOption, value);
                     looping = false;
                     break;
                 default:
                     System.out.println("Selecione uma opção válida!");
             }
         }
-        return ticketOption;
-
     }
 
-    public TicketOption getTicketOption() {
-        return this.ticketOption;
+    private double totalValueCalculate(int quantity, TicketOption ticketOption){
+      double value = quantity * ticketOption.getPrice();
+      return value;
+    }
+
+    public double getValue(){
+        return this.value;
+    }
+
+    public HashMap<TicketOption, Double> getTicket() {
+        return ticket;
     }
 }
